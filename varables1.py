04b52038bc02409bed0cd44c6b1835870caebdd0
Kothapally Gnaneswar <kothapallignaneswar05@gmail.com>
2026-06-06 15:03:15 +0530

    Add files via upload

---
+++ varables1.py
@@ -0,0 +1,11 @@
+first_name="Gnaneswar"
+last_name="Kothapally"
+full_name=last_name+" "+first_name
+print(full_name)
+age=input("Enter your age:")
+College=input("Enter your college name:")
+bio=input("Write a short bio about yourself:")
+print("Your name is "+full_name)
+print("Your age is "+age)                                   
+print("Your college name is "+College)
+print("Your bio is "+bio)
