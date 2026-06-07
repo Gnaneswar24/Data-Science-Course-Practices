d2adbc02c910aeb911d8bcece316bf41fb6b1e69
Kothapally Gnaneswar <kothapallignaneswar05@gmail.com>
2026-06-07 14:45:24 +0530

    Add files via upload

---
+++ Math modules.py
@@ -0,0 +1,13 @@
+from math import* 
+c=sqrt(2)
+print(c)    
+d=pow(2,3)
+print(d)     
+e=log(10)
+print(e)
+a=round(3.14159,2)
+print(a)    
+x=8.94427190999916
+y=int(x)    
+print(ceil(x))
+print(floor(x))
