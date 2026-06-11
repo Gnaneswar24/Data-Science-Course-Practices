c66db0dec1a6a986062cdf1419ae4b16e6693544
Kothapally Gnaneswar <kothapallignaneswar05@gmail.com>
2026-06-11 15:38:52 +0530

    Add files via upload

---
+++ Operaters.py
@@ -0,0 +1,50 @@
+a=6
+b=4
+#Arithmetic Operater
+c=a+b
+e=a*b
+d=a-b
+f=a/b
+g=a//b
+h=a%b
+#Relational Operater
+i=(a==b)
+j=(a!=b)
+k=(a>b)
+l=(a<b)
+print(c,d,e,f,g,h,i,j,k,l)
+#Assignment Operator
+aa = 10
+aa += 5
+print(aa)
+aa -= 3
+print(aa)
+aa *= 2
+print(aa)
+aa /= 4
+print(aa)
+aa //= 2
+print(aa)
+aa %= 1
+print(aa)
+#Logical Operator
+bb=10
+cc=11
+x=True
+m=cc<10 and bb>10
+n=cc<10 or bb==10
+o=not x
+print(m,n,o)
+#bitwise Operator
+aaa=4
+bbb=5
+ccc=7
+ab=~aaa
+ac=aaa<4 & bbb>3
+ad=aaa<4 | bbb>3
+ae=aaa<4 ^ bbb>3
+af=12<<1
+ag=12>>1
+print(bin(24))
+print(bin(12))
+print(ac,ab,ae,af,ag)
